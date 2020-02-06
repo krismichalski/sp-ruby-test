26f180a871f2960f414310827c9ef198d16d4110
Kristopher Michalski <kristopher.michalski+git@gmail.com>
2020-02-06 21:48:16 +0100

    Give bin/ script help and options

--- bin/parser.rb
+++ bin/parser.rb
@@ -3,8 +3,29 @@
 # frozen_string_literal: true
 
 require_relative '../lib/parser'
+require 'optparse'
+require 'ostruct'
 
-puts "TOTAL:"
-Parser.new(file_path: ARGV[0], mode: :total).call
-puts "UNIQUE:"
-Parser.new(file_path: ARGV[0], mode: :unique).call
+options = OpenStruct.new
+options.mode = :total
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: parser.rb <path to file> [options]"
+  opts.separator ""
+  opts.separator "Specific options:"
+
+  opts.on("--mode [MODE]", [:total, :unique], "Select mode: 'total' (default) or 'unique'") do |mode|
+    options.mode = mode
+  end
+
+  opts.separator ""
+  opts.separator "Common options:"
+
+  opts.on_tail("-h", "--help", "Show this message") do
+    puts opts
+    exit
+  end
+end.parse!
+
+puts "#{options[:mode].to_s.upcase}:"
+Parser.new(file_path: ARGV[0], mode: options[:mode]).call
