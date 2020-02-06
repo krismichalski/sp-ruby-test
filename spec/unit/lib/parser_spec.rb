7e453d5d81d32a33f573acc40a8b734e52bcf862
Kristopher Michalski <kristopher.michalski+git@gmail.com>
2020-02-06 20:51:11 +0100

    Modes: failing test

--- spec/unit/lib/parser_spec.rb
+++ spec/unit/lib/parser_spec.rb
@@ -1,31 +1,45 @@
 require 'parser'
 
 RSpec.describe Parser do
-  let(:parser) { described_class.new(file_path: file_path) }
+  let(:parser) { described_class.new(file_path: file_path, mode: mode) }
   let(:file_path) { "webserver.log" }
 
   describe "#call" do
     subject { parser.call }
 
-    it "prints out sorted visits" do
-      expect { subject }.to output(
-        <<~TEXT
-          "================ SORTED =================="
-          "/about/2 90 visits"
-          "/contact 89 visits"
-          "/index 82 visits"
-          "/about 81 visits"
-          "/help_page/1 80 visits"
-          "/home 78 visits"
-          "================ UNIQUE =================="
-          "/help_page/1 23 unique visits"
-          "/contact 23 unique visits"
-          "/home 23 unique visits"
-          "/index 23 unique visits"
-          "/about/2 22 unique visits"
-          "/about 21 unique visits"
-        TEXT
-      ).to_stdout
+    context "sort by total hits" do
+      let(:mode) { :total }
+
+      it "prints out sorted visits" do
+        expect { subject }.to output(
+          <<~TEXT
+            /about/2 90 visits
+            /contact 89 visits
+            /index 82 visits
+            /about 81 visits
+            /help_page/1 80 visits
+            /home 78 visits
+          TEXT
+        ).to_stdout
+      end
     end
+
+    context "sort by unique hits" do
+      let(:mode) { :unique }
+
+      it "prints out sorted visits" do
+        expect { subject }.to output(
+          <<~TEXT
+            /help_page/1 23 visits
+            /contact 23 visits
+            /home 23 visits
+            /index 23 visits
+            /about/2 22 visits
+            /about 21 visits
+          TEXT
+        ).to_stdout
+      end
+    end
+
   end
 end
