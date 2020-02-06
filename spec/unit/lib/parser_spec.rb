7d569850abe665ebbae2737d76f1b997b07276ab
Kristopher Michalski <kristopher.michalski+git@gmail.com>
2020-02-06 20:41:40 +0100

    Write simple test for Parser

--- spec/unit/lib/parser_spec.rb
+++ spec/unit/lib/parser_spec.rb
@@ -1,4 +1,31 @@
 require 'parser'
 
 RSpec.describe Parser do
+  let(:parser) { described_class.new(file_path: file_path) }
+  let(:file_path) { "webserver.log" }
+
+  describe "#call" do
+    subject { parser.call }
+
+    it "prints out sorted visits" do
+      expect { subject }.to output(
+        <<~TEXT
+          "================ SORTED =================="
+          "/about/2 90 visits"
+          "/contact 89 visits"
+          "/index 82 visits"
+          "/about 81 visits"
+          "/help_page/1 80 visits"
+          "/home 78 visits"
+          "================ UNIQUE =================="
+          "/help_page/1 23 unique visits"
+          "/contact 23 unique visits"
+          "/home 23 unique visits"
+          "/index 23 unique visits"
+          "/about/2 22 unique visits"
+          "/about 21 unique visits"
+        TEXT
+      ).to_stdout
+    end
+  end
 end
